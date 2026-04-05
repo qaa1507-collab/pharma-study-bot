8dc5b9393ee2962333e9683acc33454831eee40b
qaa1507-collab <qaa1507@hanyang.ac.kr>
2026-04-05 21:47:04 +0900

    Refactor imports and update text splitter module

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from datetime import datetime
 from langchain_openai import ChatOpenAI, OpenAIEmbeddings
 from langchain_community.vectorstores import Chroma
 from langchain_community.document_loaders import PyPDFLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain.chains import RetrievalQA
 from langchain.prompts import PromptTemplate
 
